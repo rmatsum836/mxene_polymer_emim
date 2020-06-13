316787ddf65fa04b380751bbb7f056ad55eb8373
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-06-13 12:24:48 -0500

    Make edits to build function

--- mxene_polymer/build_functions/aa_mxenes.py
+++ mxene_polymer/build_functions/aa_mxenes.py
@@ -10,46 +10,35 @@ from mtools.gromacs.gromacs import parse_nonbond_params
 from mbuild.formats.lammpsdata import write_lammpsdata
 
 def build_alkylammonium_mxene(n_compounds, composition, periods, chain_length=12,
-        dspacing=1.1):
+        displacement=1.1):
     ti3c2 = build_structure(periods=periods,
             composition=composition,
-            dspacing=dspacing,
+            displacement=displacement,
             lateral_shift=True,
             atomtype=True)
    
     n_carbons = 'C' * chain_length
-    aa = mb.load(f'{n_carbons}N(C)(C)C', smiles=True)
+    aa = mb.load(f'{n_carbons}[N](C)(C)C', smiles=True)
     aa.name = 'alkylam'
     
     lopes = get_ff('lopes')
     
     region1 = mb.Box(mins=[0,
                            0,
-                           (ti3c2.box[2] / 10 - 2 * 0.5) / 4 + 0.15],
+                           (ti3c2.box[2] / 10 - 2 * displacement) / 2 + 0.15],
                      maxs=[ti3c2.box[0] / 10,
                            ti3c2.box[1] / 10,
-                           (ti3c2.box[2] / 10 -2 * 0.5) / 4 + 1.15])
-    
-    region2 = mb.Box(mins=[0,
-                           0,
-                           (ti3c2.box[2] / 10 - 2 * 0.5) * 3 / 4 + 0.5 + 0.15],
-                     maxs=[ti3c2.box[0] / 10,
-                           ti3c2.box[1] / 10,
-                           (ti3c2.box[2] / 10 - 2 * 0.5) * 3 / 4 + 1.65])
+                           (ti3c2.box[2] / 10 - 2 * displacement) / 2 + displacement])
     
     aa_1 = mb.fill_box(
         compound=aa,
         n_compounds=n_compounds,
         box=region1,
         fix_orientation=True)
-    
-    water2 = mb.fill_box(
-        compound=aa,
-        n_compounds=n_compounds,
-        box=region2,
-        fix_orientation=True)
-    
-    aaPM = lopes.apply(aa, residues=['alkylam'])
+        
+    aaPM = lopes.apply(aa_1, residues=['alkylam'], assert_bond_params=False,
+            assert_angle_params=False, assert_dihedral_params=False)
     system = aaPM + ti3c2
-    
+   
+    system.save('ti3c2.gro', combine='all', overwrite=True)
     write_lammpsdata(system, 'data.mxene')
--- mxene_polymer/simulations/aa/12/build_mxene.py
+++ mxene_polymer/simulations/aa/12/build_mxene.py
@@ -1,13 +1,13 @@
-from build_functions.aa_mxenes import build_alkylammonium_mxene
+from mxene_polymer.build_functions.aa_mxenes import build_alkylammonium_mxene
 
 composition = {'O': 1}
-chain_length = 12
+chain_length = 1
 periods = [12, 12, 1]
-dspacing = 1.1
-n_compounds = 50
+displacement = 1.1
+n_compounds = 20
 
 build_alkylammonium_mxene(chain_length=chain_length,
-        dspacing=dspacing,
+        displacement=displacement,
         n_compounds=n_compounds,
         composition=composition,
         periods=periods)

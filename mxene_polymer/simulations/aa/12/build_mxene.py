5ab24233cbedf5d6aba7fc6782df36a9b7a9fdf3
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-06-15 17:13:15 -0500

    Add second fluid region

--- mxene_polymer/simulations/aa/12/build_mxene.py
+++ mxene_polymer/simulations/aa/12/build_mxene.py
@@ -4,7 +4,7 @@ composition = {'OH': 1}
 chain_length = 12
 periods = [20, 20, 1]
 #displacement = 1.1
-displacement = 5
+displacement = 1.5
 n_compounds = 50
 
 build_alkylammonium_mxene(chain_length=chain_length,

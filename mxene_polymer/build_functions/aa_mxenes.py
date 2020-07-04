22c05c5242048b0a3d2086c123ad4fa45cfbc817
Ray A. Matsumoto <ray.a.matsumoto@vanderbilt.edu>
2020-07-04 10:28:18 -0500

    Fix fill box in build functions

--- mxene_polymer/build_functions/aa_mxenes.py
+++ mxene_polymer/build_functions/aa_mxenes.py
@@ -1,4 +1,4 @@
-from mxenes.structures import build_structure, change_charge
+from mxenes.structures import build_structure, change_charge, _apply_nbfixes, collapse_atomtypes
 from mxenes.utils.utils import get_fn
 
 import numpy as np
@@ -20,24 +20,22 @@ def build_alkylammonium_mxene(n_compounds, composition, periods, chain_length=12
     n_carbons = 'C' * chain_length
     aa = mb.load(f'{n_carbons}[N](C)(C)C', smiles=True)
 
-    emim = mb.load(get_il('emim'))
-    emim.name = 'emim'
 
     # Rename some carbons
     # CE = end carbon
     # CM = middle carbon
     # CB = branch carbon
-    for idx, particle in enumerate(aa.particles()):
-        if idx == 0:
-            particle.name = 'C_E'
-        if idx == chain_length / 2:
-            particle.name = 'C_M'
-        # TODO: Find way to get branch carbon index without
-        # hardcoding
-        if idx == 13:
-            particle.name = 'C_B1'
-        if idx == 14:
-            particle.name = 'C_B2'
+    #for idx, particle in enumerate(aa.particles()):
+    #    if idx == 0:
+    #        particle.name = 'C_E'
+    #    if idx == chain_length / 2:
+    #        particle.name = 'C_M'
+    #    # TODO: Find way to get branch carbon index without
+    #    # hardcoding
+    #    if idx == 13:
+    #        particle.name = 'C_B1'
+    #    if idx == 14:
+    #        particle.name = 'C_B2'
 
     aa.name = 'alkylam'
     
@@ -57,13 +55,13 @@ def build_alkylammonium_mxene(n_compounds, composition, periods, chain_length=12
                            (ti3c2.box[2] / 10 - 2 * displacement) + 2 + displacement])
     
     aa_1 = mb.fill_box(
-        compound=[aa, emim],
+        compound=aa,
         n_compounds=n_compounds,
         box=region1,
         fix_orientation=True)
 
     aa_2 = mb.fill_box(
-        compound=[aa, emim],
+        compound=aa,
         n_compounds=n_compounds,
         box=region2,
         fix_orientation=True)
@@ -74,6 +72,8 @@ def build_alkylammonium_mxene(n_compounds, composition, periods, chain_length=12
             assert_dihedral_params=False)
 
     system = aa1PM + aa2PM + ti3c2
+    system = _apply_nbfixes(system)
+    system = collapse_atomtypes(system)
     change_charge(system, new_charge=0)
     system.box = ti3c2.box
     for atom in system.atoms:
@@ -117,21 +117,8 @@ def build_tam_emim_mxene(n_compounds, composition, periods, chain_length=12,
     n_carbons = 'C' * chain_length
     aa = mb.load(f'{n_carbons}[N](C)(C)C', smiles=True)
 
-    # Rename some carbons
-    # CE = end carbon
-    # CM = middle carbon
-    # CB = branch carbon
-    for idx, particle in enumerate(aa.particles()):
-        if idx == 0:
-            particle.name = 'C_E'
-        if idx == chain_length / 2:
-            particle.name = 'C_M'
-        # TODO: Find way to get branch carbon index without
-        # hardcoding
-        if idx == 13:
-            particle.name = 'C_B1'
-        if idx == 14:
-            particle.name = 'C_B2'
+    emim = mb.load(get_il('emim'))
+    emim.name = 'emim'
 
     aa.name = 'alkylam'
     
@@ -151,34 +138,27 @@ def build_tam_emim_mxene(n_compounds, composition, periods, chain_length=12,
                            (ti3c2.box[2] / 10 - 2 * displacement) + 2 + displacement])
     
     aa_1 = mb.fill_box(
-        compound=aa,
+        compound=[aa, emim],
         n_compounds=n_compounds,
         box=region1,
         fix_orientation=True)
 
     aa_2 = mb.fill_box(
-        compound=aa,
+        compound=[aa, emim],
         n_compounds=n_compounds,
         box=region2,
         fix_orientation=True)
         
-    aa1PM = lopes.apply(aa_1, residues=['alkylam'],
+    aa1PM = lopes.apply(aa_1, residues=['alkylam', 'emim'],
             assert_dihedral_params=False)
-    aa2PM = lopes.apply(aa_2, residues=['alkylam'],
+    aa2PM = lopes.apply(aa_2, residues=['alkylam', 'emim'],
             assert_dihedral_params=False)
 
     system = aa1PM + aa2PM + ti3c2
+    system = _apply_nbfixes(system)
+    system = collapse_atomtypes(system)
     change_charge(system, new_charge=0)
     system.box = ti3c2.box
-    for atom in system.atoms:
-        if atom.name == 'C_E':
-            atom.name = 'CE'
-        elif atom.name == 'C_M':
-            atom.name = 'CM'
-        elif atom.name == 'C_B1':
-            atom.name = 'CB1'
-        elif atom.name == 'C_B2':
-            atom.name = 'CB2'
    
     system.save('ti3c2.gro', combine='all', overwrite=True)
     system.save('ti3c2.top', combine='all', overwrite=True)

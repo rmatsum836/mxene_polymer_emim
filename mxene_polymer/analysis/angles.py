2a8e4fcff9ba9e825678aaa4a05820a6c9a31ae8
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2020-09-16 18:57:19 -0400

    Update angle analysis for ring

--- mxene_polymer/analysis/angles.py
+++ mxene_polymer/analysis/angles.py
@@ -18,9 +18,16 @@ def angle_between(v1, v2):
     return theta 
 
 
-def compute_angles(top_file, trj_file):
+def compute_angles(top_file, trj_file, tam_length):
     """ Compute angles between specific groups and normal of the MXene surface
         WARNING: Do not use Gromacs XTC file as lower precision will result in noise around 90 degrees
+
+    top_file : str
+        A topology file that contains atomtype information
+    trj_file : str
+        A trajectory file that is valid with MDAnalysis
+    tam_length : int
+        tetraalkylammonium length in box
     """
     universe = MDAnalysis.Universe(top_file, trj_file)
 
@@ -28,8 +35,8 @@ def compute_angles(top_file, trj_file):
     
     box = universe.select_atoms('all').bbox()
 
-    #ring_groups = [universe.select_atoms('type kpl_012 kpl_011 and resid {}'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
-    ring_groups = [universe.select_atoms('type lopes_012 lopes_011 and resid {}'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
+    ring_groups = [universe.select_atoms('type kpl_012 kpl_011 and resid {}'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
+    #ring_groups = [universe.select_atoms('type lopes_012 lopes_011 and resid {}'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
 
     ring_angles = np.nan * np.empty(shape=(universe.trajectory.n_frames, len(ring_groups)))
     """
@@ -42,26 +49,26 @@ def compute_angles(top_file, trj_file):
     for n_frame, frame in enumerate(universe.trajectory[::1]):
         for n_ring, ring_group in enumerate(ring_groups):
             # Consider first layer of ions
-            #if ring_group.center_of_mass()[2] > 28.47 + 5.0 or ring_group.center_of_mass()[2] < 28.47:
-            if ring_group.center_of_mass()[2] > 6.82 + 5.0:
-                continue 
+            #if ring_group.center_of_mass()[2] > 6.82 + 5.0:
+            if tam_length == 12:
+                z_length = 28.37
+            elif tam_length == 16:
+                z_length = 32.62
+            pore1 = ring_group.center_of_mass()[2] > z_length + 5.0 or ring_group.center_of_mass()[2] < z_length
+            pore2 = ring_group.center_of_mass()[2] > 9.37 + 5.0 or ring_group.center_of_mass()[2] < 9.37
+            if not pore1 or not pore2:
+                continue
             # Only consider ions in the pore
-            #if ring_group.center_of_mass()[1] < 1.5 or ring_group.center_of_mass()[1] > 5.5:
-            #    continue
+            if ring_group.center_of_mass()[1] < 5 or ring_group.center_of_mass()[1] > 45:
+                continue
             print(n_ring)
             xyz = ring_group.positions 
-            # vector of ring com x,y, and normal of ring
-            tmp_A = []
-            # ring com z coordinate
-            tmp_b = []
-            for i in range(len(xyz)):
-                tmp_A.append([xyz[i, 0], xyz[i, 1], 1])
-                tmp_b.append(xyz[i, 2])
-            b = np.matrix(tmp_b).T
-            A = np.matrix(tmp_A)
-            fit = (A.T * A).I * A.T * b
-            fit = np.array(fit.T)[0]
-            ring_angles[n_frame, n_ring] = angle_between([fit[0], fit[1], -1], [0, 0, 1]) * (180 / np.pi)
+            #AB = xyz[0, :] - ring_group.center_of_mass()
+            #AC = xyz[1, :] - ring_group.center_of_mass()
+            AB = xyz[0, :] - xyz[2, :]
+            AC = xyz[1, :] - xyz[2, :]
+            plane_vector = np.cross(AB, AC) 
+            ring_angles[n_frame, n_ring] = angle_between([plane_vector[0], plane_vector[1], plane_vector[2]], [0, 0, 1]) * (180 / np.pi)
 
     ring_angles = ring_angles.reshape((-1))
     ring_angles = ring_angles[np.logical_not(np.isnan(ring_angles))]

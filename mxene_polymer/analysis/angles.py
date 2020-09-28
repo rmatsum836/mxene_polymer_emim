6978b96a8e5d739fca8a73bab91ba1f536fb3901
Ray Matsumoto <raymat@master.cl.vanderbilt.edu>
2020-09-28 14:19:51 -0400

    Update angle code to calculate tail angles

--- mxene_polymer/analysis/angles.py
+++ mxene_polymer/analysis/angles.py
@@ -34,9 +34,11 @@ def compute_angles(top_file, trj_file, tam_length):
     #ring_carbon = universe.select_atoms('type kpl_012 kpl_011')
     
     box = universe.select_atoms('all').bbox()
+    _compute_ring_angles(universe, tam_length)
 
+
+def _compute_ring_angles(universe, tam_length):
     ring_groups = [universe.select_atoms('type kpl_012 kpl_011 and resid {}'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
-    #ring_groups = [universe.select_atoms('type lopes_012 lopes_011 and resid {}'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
 
     ring_angles = np.nan * np.empty(shape=(universe.trajectory.n_frames, len(ring_groups)))
     """
@@ -45,7 +47,6 @@ def compute_angles(top_file, trj_file, tam_length):
     >>> np.where(np.isnan(x))[0].shape[0]
     4
     """
-
     for n_frame, frame in enumerate(universe.trajectory[::1]):
         for n_ring, ring_group in enumerate(ring_groups):
             # Consider first layer of ions
@@ -54,8 +55,8 @@ def compute_angles(top_file, trj_file, tam_length):
                 z_length = 28.37
             elif tam_length == 16:
                 z_length = 32.62
-            pore1 = ring_group.center_of_mass()[2] > z_length + 5.0 or ring_group.center_of_mass()[2] < z_length
-            pore2 = ring_group.center_of_mass()[2] > 9.37 + 5.0 or ring_group.center_of_mass()[2] < 9.37
+            pore1 = ring_group.center_of_mass()[2] > z_length or ring_group.center_of_mass()[2] < z_length + 5.0
+            pore2 = ring_group.center_of_mass()[2] > 9.37 or ring_group.center_of_mass()[2] < 9.37 + 5.0
             if not pore1 or not pore2:
                 continue
             # Only consider ions in the pore
@@ -72,11 +73,65 @@ def compute_angles(top_file, trj_file, tam_length):
 
     ring_angles = ring_angles.reshape((-1))
     ring_angles = ring_angles[np.logical_not(np.isnan(ring_angles))]
-    histo = np.histogram(ring_angles, bins=181)
-    np.savetxt('histo_ring_angles.txt', np.transpose(np.vstack([histo[0], histo[1][1:]])),
+    histo = np.histogram(ring_angles, bins=181, density=True)
+    new_bins = get_center(histo[1])
+    np.savetxt('histo_ring_angles.txt', np.transpose(np.vstack([new_bins, histo[0]])),
         header='Count\tAngle')
     np.savetxt('ring_angles.txt', np.asarray(ring_angles))
     fig, ax = plt.subplots()
-    ax.hist(ring_angles, bins=181)
+    ax.hist(ring_angles, bins=181, density=True)
     plt.xlim((0, 180))
     fig.savefig('ring_angles.pdf')
+
+
+def _compute_tail_angles(universe, tam_length):
+    tail_groups = [universe.select_atoms('type kpl_014 kpl_016 and resid {} and not bonded type lopes_016'.format(r.resid)) for r in universe.residues if r.resname == 'emim']
+
+    tail_angles = np.nan * np.empty(shape=(universe.trajectory.n_frames, len(tail_groups)))
+    """
+    >>> np.where(np.isnan([4, 4, ]))[0].shape[0]
+    0
+    >>> np.where(np.isnan(x))[0].shape[0]
+    4
+    """
+
+    for n_frame, frame in enumerate(universe.trajectory[::1]):
+        for n_tail, tail_group in enumerate(tail_groups):
+            # Consider first layer of ions
+            #if ring_group.center_of_mass()[2] > 6.82 + 5.0:
+            if tam_length == 12:
+                z_length = 28.37
+            elif tam_length == 16:
+                z_length = 32.62
+            elif tam_length == 'channel':
+                z_length = 6.82
+            pore1 = tail_group.center_of_mass()[2] > z_length and tail_group.center_of_mass()[2] < (z_length+7.0)
+            if not pore1:
+                continue
+            #print(n_tail)
+            xyz = tail_group.positions 
+            plane_vector = xyz[1] - xyz[0]
+            tail_angles[n_frame, n_tail] = angle_between([plane_vector[0], plane_vector[1], plane_vector[2]], [0, 0, 1]) * (180 / np.pi)
+
+    tail_angles = tail_angles.reshape((-1))
+    tail_angles = tail_angles[np.logical_not(np.isnan(tail_angles))]
+    histo = np.histogram(tail_angles, bins=181, density=True)
+    new_bins = get_center(histo[1])
+    np.savetxt('histo_tail_angles.txt', np.transpose(np.vstack([histo[1][1:], histo[0]])),
+        header='Count\tAngle')
+    np.savetxt('tail_angles.txt', np.asarray(tail_angles))
+    fig, ax = plt.subplots()
+    ax.hist(tail_angles, density=True, bins=181)
+    plt.xlim((0, 180))
+    plt.ylim((0, 0.06))
+    fig.savefig('tail_angles.pdf')
+
+def get_center(bins):
+    new_bins = list()
+    for idx, bi in enumerate(bins):
+        if (idx+1) >= len(bins):
+            continue
+        mid = (bins[idx] + bins[idx+1])/2
+        new_bins.append(mid)
+
+    return new_bins
